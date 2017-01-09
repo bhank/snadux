938f66b5f79c8c7c041396000eb6468514d54959
Adam Coyne <github@mail2.coyne.nu>
2017-01-08 23:27:58 -0600

    Disable devtools in case of performance impact

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 import Game from './components/Game';
-//import logo from './logo.svg';
 import './App.css';
 
-
- const middleware = [ thunkMiddleware ];
- const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
- const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
-//const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+let store;
+const USE_REDUX_DEVTOOLS = false;
+if(USE_REDUX_DEVTOOLS) {
+  const middleware = [ thunkMiddleware ];
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
+} else {
+  store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+}
 
 const App = () => (
-	<Provider store={store}>
-		<div>
-			<Game />
-		</div>
-	</Provider>
+  <Provider store={store}>
+    <div>
+      <Game />
+    </div>
+  </Provider>
 );
 
 export default App;
