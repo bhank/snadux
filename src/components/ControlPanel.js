ce556888b8ef8e004bb3d752190831352005a729
Adam Coyne <github@mail2.coyne.nu>
2017-01-09 00:08:21 -0600

    Update pause button text and disabled

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { restartGame, setPauseState } from '../actions';
+import * as constants from '../constants';
 
-const ControlPanel = ({ dispatch }) => (
+const ControlPanel = ({ dispatch, disablePauseButton, paused }) => (
     <div>
-        <button onClick={() => dispatch(setPauseState())}>Pause/Unpause</button>
+        <button disabled={disablePauseButton} onClick={() => dispatch(setPauseState())}>{paused ? 'Resume' : 'Pause'}</button>
         <button onClick={() => dispatch(restartGame())}>Restart</button>
     </div>
 );
 
-export default connect()(ControlPanel);
+function mapStateToProps(state) {
+    return {
+        disablePauseButton: state.status !== constants.STATUS_PAUSED && state.status !== constants.STATUS_PLAYING,
+        paused: state.status === constants.STATUS_PAUSED
+    }
+}
+export default connect(mapStateToProps)(ControlPanel);
