c9be0ea1d91edb353d18bfda187be02362f25166
Adam Coyne <github@mail2.coyne.nu>
2017-01-09 09:39:06 -0600

    Add "game over" message with restart link

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -3,17 +3,20 @@ import { connect } from 'react-redux';
 import { restartGame, setPauseState } from '../actions';
 import * as constants from '../constants';
 
-const ControlPanel = ({ dispatch, disablePauseButton, paused }) => (
+const ControlPanel = ({ dispatch, disablePauseButton, paused, gameOver }) => (
     <div>
         <button disabled={disablePauseButton} onClick={() => dispatch(setPauseState())}>{paused ? 'Resume' : 'Pause'}</button>
         <button onClick={() => dispatch(restartGame())}>Restart</button>
+        <span style={{display: gameOver ? 'inline' : 'none' }}> Game Over! Click <a href="#" onClick={(e) => { e.preventDefault(); dispatch(restartGame()); } }>Restart</a> to try again.</span>
     </div>
 );
 
 function mapStateToProps(state) {
     return {
         disablePauseButton: state.status !== constants.STATUS_PAUSED && state.status !== constants.STATUS_PLAYING,
-        paused: state.status === constants.STATUS_PAUSED
+        paused: state.status === constants.STATUS_PAUSED,
+        gameOver: state.status === constants.STATUS_GAME_OVER
     }
 }
+
 export default connect(mapStateToProps)(ControlPanel);
